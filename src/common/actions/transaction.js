39e71a6e1836d943104c23cd3e2adba89abca062
Fred Souza <fredericoamsouza@gmail.com>
2017-10-27 02:05:37 -0200

    Improving error handling

--- src/common/actions/transaction.js
+++ src/common/actions/transaction.js
@@ -15,7 +15,11 @@ export const history = () => (dispatch) => {
     const { wallet } = store.getState().wallet;
     const walletAddress = wallet.getAddress();
     axios.get(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=0&toBlock=latest&address=${CONTRACT_ADDRESS}&topic0=${OPERATION_TRANSFER_HASH}&topic1=${walletAddress}&topic0_1_opr=or&topic2=${walletAddress}&topic0_2_opr=or`)
-        .then(({ data: { result } }) => dispatch({ type: HISTORY, payload: result }));
+        .then(({ status, data }) => {
+            if (status === 200 && data.status === "1") {
+                dispatch({ type: HISTORY, payload: data.result });
+            }
+        });
 }
 
 export const isLoading = (loading) => ({ type: LOADING, payload: !!loading });
