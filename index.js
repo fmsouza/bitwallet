6943e0bda75f48470ad04ce80a3f69cf014f1486
Fred Souza <fredericoamsouza@gmail.com>
2017-10-27 18:51:42 -0200

    Shimming Uint8Array.prototype.slice

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import { AppRegistry } from 'react-native';
 import App from './src';
 
+if (!Uint8Array.prototype.slice) {
+    Uint8Array.prototype.slice = function() {
+        var args = Array.prototype.slice.call(arguments);
+        return new Uint8Array(Array.prototype.slice.apply(this, args));
+    }
+}
+
 AppRegistry.registerComponent('BitWallet', () => App);
