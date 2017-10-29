18a35eb7e718fd3a3cdf95ae4b91de65580ebd70
Fred Souza <fredericoamsouza@gmail.com>
2017-10-29 00:25:49 -0200

    Simplifying history request call

--- src/common/actions/transaction.js
+++ src/common/actions/transaction.js
@@ -2,14 +2,9 @@ import ethers from 'ethers';
 import axios from 'axios';
 import { GAS_LIMIT as gasLimit, GAS_PRICE as gasPrice, TRANSACTION_ACTIONS, OPERATION_TRANSFER_HASH, CONTRACT_ADDRESS } from 'common/constants';
 import store from 'common/stores';
+import { padStartHex } from 'common/utils';
 
 const { HISTORY, LOADING, TRANSFER } = TRANSACTION_ACTIONS;
-
-const filterTransactions = (walletAddress) => (tx) => {
-    const topics = tx.topics.map(topic => parseInt(topic));
-    const me = parseInt(walletAddress);
-    return topics.indexOf(me) > -1;
-}
 
 export const transfer = (amount, to) => (dispatch) => {
     const { contract, wallet } = store.getState().wallet;
@@ -19,13 +14,13 @@ export const transfer = (amount, to) => (dispatch) => {
 
 export const history = () => (dispatch) => {
     const { wallet } = store.getState().wallet;
-    const walletAddress = wallet.getAddress();
-    axios.get(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=0&toBlock=latest&address=${CONTRACT_ADDRESS}&topic0=${OPERATION_TRANSFER_HASH}&topic1=${walletAddress}&topic0_1_opr=or&topic2=${walletAddress}&topic0_2_opr=or`)
+    const walletAddress = padStartHex(wallet.getAddress()).toLowerCase();
+    axios.get(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=0&toBlock=latest&address=${CONTRACT_ADDRESS}&topic0=${OPERATION_TRANSFER_HASH}&topic1=${walletAddress}&topic2=${walletAddress}&topic1_2_opr=or`)
         .then(({ status, data }) => {
-            if (status === 200 && data.status === "1") {
-                const payload = data.result.filter(filterTransactions(walletAddress));
-                dispatch({ type: HISTORY, payload });
-            }
+            if (status === 200 && data.status == 1)
+                dispatch({ type: HISTORY, payload: data.result });
+            else
+                dispatch({ type: LOADING, payload: false });
         });
 }
 
--- src/common/utils/wallet.js
+++ src/common/utils/wallet.js
@@ -1,3 +1,8 @@
 import ethers from 'ethers';
 
 export const generateMnemonics = () => ethers.HDNode.entropyToMnemonic(ethers.utils.randomBytes(16));
+
+export const padStartHex = (hex, targetLength = 64, padString = '0') => {
+    if (!hex.indexOf('0x')) hex = hex.slice(2);
+    return `0x${hex.padStart(targetLength, padString)}`;
+}
