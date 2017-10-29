0eb5dfcd3bb7a3b9f9991969df1fac32dc157781
Fred Souza <fredericoamsouza@gmail.com>
2017-10-29 15:19:09 -0200

    Making the transaction cheaper

--- src/common/constants/transaction.js
+++ src/common/constants/transaction.js
@@ -4,6 +4,7 @@ export const TRANSACTION_ACTIONS = {
     LOADING: 'ACTION/TRANSACTION/LOADING'
 };
 
-export const GAS_LIMIT = 40000;
-export const GAS_PRICE = 0xBEBC200;
+export const GAS_LIMIT = 60000;
+// export const GAS_PRICE = 0xBEBC200; // 0.2 gwei
+export const GAS_PRICE = 0x5F5E100; // 0.1 gwei
 export const OPERATION_TRANSFER_HASH = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
