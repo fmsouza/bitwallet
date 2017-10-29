7c9186868f377f50c6bcef662c6ffcbbea20ac90
Fred Souza <fredericoamsouza@gmail.com>
2017-10-29 00:25:09 -0200

    Moving shims to a separated file

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 import { AppRegistry } from 'react-native';
+import 'common/utils/shims';
 import App from './src';
-
-if (!Uint8Array.prototype.slice) {
-    Uint8Array.prototype.slice = function() {
-        var args = Array.prototype.slice.call(arguments);
-        return new Uint8Array(Array.prototype.slice.apply(this, args));
-    }
-}
 
 AppRegistry.registerComponent('BitWallet', () => App);
---
+++ src/common/utils/shims.js
@@ -0,0 +1,25 @@
+if (!Uint8Array.prototype.slice) {
+    Uint8Array.prototype.slice = function() {
+        var args = Array.prototype.slice.call(arguments);
+        return new Uint8Array(Array.prototype.slice.apply(this, args));
+    }
+}
+
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+        padString = String(padString || ' ');
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
