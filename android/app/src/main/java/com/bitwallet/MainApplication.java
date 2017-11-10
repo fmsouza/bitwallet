7856e9212ea56d5f3eb2f3026e4f993cb026e5fa
Fred Souza <fredericoamsouza@gmail.com>
2017-11-09 23:08:01 -0200

    Updating environment

--- android/app/src/main/java/com/bitwallet/MainApplication.java
+++ android/app/src/main/java/com/bitwallet/MainApplication.java
@@ -3,8 +3,8 @@ package com.bitwallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
 import com.horcrux.svg.SvgPackage;
+import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSensitiveInfoPackage(),
             new SvgPackage(),
+            new RNSensitiveInfoPackage(),
             new RCTCameraPackage()
       );
     }
