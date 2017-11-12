4b6ce3513896c13ba01e32d9855beb6b90b5f40f
Fred Souza <fredericoamsouza@gmail.com>
2017-11-12 18:29:17 -0200

    Using Math lib to do pow operation

--- src/common/utils/wallet.js
+++ src/common/utils/wallet.js
@@ -1,11 +1,13 @@
 import ethers from 'ethers';
 import { BLOCKCHAIN_DECIMALS } from 'common/constants';
 
+const decimals = Math.pow(10, BLOCKCHAIN_DECIMALS);
+
 export const padStartHex = (hex, targetLength = 64, padString = '0') => {
     if (!hex.indexOf('0x')) hex = hex.slice(2);
     return `0x${hex.padStart(targetLength, padString)}`;
 }
 
-export const tokenDecimals = (value) => value / (10**BLOCKCHAIN_DECIMALS);
+export const tokenDecimals = (value) => value / decimals;
 
-export const expandTokenAmount = (value) => value * (10**BLOCKCHAIN_DECIMALS);
+export const expandTokenAmount = (value) => value * decimals;
