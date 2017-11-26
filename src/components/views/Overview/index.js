32b869bebe028bfe1db5d03aae80c53426ba22c0
Fred Souza <fredericoamsouza@gmail.com>
2017-11-26 01:39:32 -0200

    Changing Overview page alignment

--- src/components/views/Overview/index.js
+++ src/components/views/Overview/index.js
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'stretch',
-        justifyContent: 'flex-start',
+        justifyContent: 'space-between',
     },
     balance: {
         height: 140,
