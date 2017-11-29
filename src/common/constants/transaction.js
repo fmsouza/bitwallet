cead740c17e9fb608d2eb00ad0b9524acfe96c18
Fred Souza <fredericoamsouza@gmail.com>
2017-11-28 23:00:26 -0200

    Using lowest limits

--- src/common/constants/transaction.js
+++ src/common/constants/transaction.js
@@ -1,8 +1,8 @@
 export const OPERATION_TRANSFER_HASH = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
 
 export const OPTIONS = {
-    gasLimit: 100000,
-    // gasLimit: 60000,
+    // gasLimit: 100000,
+    gasLimit: 60000,
     // gasPrice: 0x5D21DBA000, // 400 gwei
     // gasPrice: 0x9502F9000, // 40 gwei
     // gasPrice: 0x12A05F200, // 5 gwei
