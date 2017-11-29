175087733af55d478bcdf178fb45c8356e7ad5f0
Fred Souza <fredericoamsouza@gmail.com>
2017-11-28 23:20:20 -0200

    Updating mocked lists

--- src/components/views/Products/mockedOffers.js
+++ src/components/views/Products/mockedOffers.js
@@ -11,7 +11,7 @@ export default [
         id: 2,
         title: 'Notebook 2 em 1 Touch Positivo Duo ZR3630 Intel',
         amount: '6768',
-        address: '0x407428BF09ea7Dac2824A64AfE88171041a02b14',
+        address: '0xf5b60c2b97b27b6fb5d5ff2c202bee62d06cfc11',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         imgUri: 'http://www.positivoinformatica.com.br/uploads/images/linhas/para-voce/notebook/DUO%20ZR/Duo_ZR_2.jpg'
     },
@@ -19,7 +19,7 @@ export default [
         id: 3,
         title: 'Smartphone Motorola Moto G5S XT1792 Platinum 32GB',
         amount: '4837',
-        address: '0x407428BF09ea7Dac2824A64AfE88171041a02b14',
+        address: '0x6b206b76d8f589a131da52ec90548c61aafc6530',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         imgUri: 'https://cdn2.gsmarena.com/vv/pics/motorola/motorola-moto-g5s-plus-1.jpg'
     },
--- src/components/views/SelectDestinationAddress/mockedContacts.js
+++ src/components/views/SelectDestinationAddress/mockedContacts.js
@@ -1,72 +1,17 @@
 export default [
     {
         id: 1,
-        name: 'Adriana Menezes',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
+        name: 'Adriano Rayol',
+        address: '0xf5b60c2b97b27b6fb5d5ff2c202bee62d06cfc11'
     },
     {
-        id: 2,
-        name: 'Adriano Marques',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
+        id: 1,
+        name: 'Carteira de teste',
+        address: '0x407428BF09ea7Dac2824A64AfE88171041a02b14'
     },
     {
         id: 3,
-        name: 'Alessandro Gasparini',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 4,
-        name: 'André Pereira',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 5,
-        name: 'Bernardo Sampaio',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 6,
-        name: 'Bruno Cardoso',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 7,
-        name: 'Daniel Braga',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 8,
-        name: 'Eleonora Fuchsa',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 9,
-        name: 'Felipe Foz',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 10,
-        name: 'Guilherme Henrique',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 11,
-        name: 'Gustavo Menezes',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 12,
-        name: 'Victor Guimarães',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 13,
-        name: 'Vitoria Oliveira',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
-    },
-    {
-        id: 14,
-        name: 'Vinicius Gusmão',
-        address: '0xG5AF0AF7GAG6D48G76DG5S987FS60AF87S4'
+        name: 'Marco Jardim',
+        address: '0x6b206b76d8f589a131da52ec90548c61aafc6530'
     }
 ];
