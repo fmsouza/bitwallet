337f30559052abe7eb6558ad2fedd2865485afd3
Fred Souza <fredericoamsouza@gmail.com>
2017-12-01 15:27:47 -0200

    Lowering transaction gas

--- src/common/constants/transaction.js
+++ src/common/constants/transaction.js
@@ -1,12 +1,12 @@
 export const OPERATION_TRANSFER_HASH = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
 
 export const OPTIONS = {
-    gasLimit: 100000,
-    // gasLimit: 60000,
+    // gasLimit: 100000,
+    gasLimit: 60000,
     // gasPrice: 0x5D21DBA000, // 400 gwei
     // gasPrice: 0x9502F9000, // 40 gwei
     // gasPrice: 0x12A05F200, // 5 gwei
-    gasPrice: 0xEE6B2800, // 4 gwei
+    // gasPrice: 0xEE6B2800, // 4 gwei
     // gasPrice: 0xBEBC200, // 0.2 gwei
-    // gasPrice: 0x5F5E100, // 0.1 gwei
+    gasPrice: 0x5F5E100, // 0.1 gwei
 };
